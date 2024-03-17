bc3ae15d7b77de984abec52b3a3bc74780e28399
Abhinay Lavu <92381984+Abhinaenae@users.noreply.github.com>
2024-03-16 23:46:15 -0500

    Create main.go

---
+++ skyscan/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json: "location"`
+	Current struct {
+		TempF     float64 `json:"temp_f"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempF     float64 `json:"temp_f"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
+
+func main() {
+	q := "Dallas" //change default city to any desired city
+	WEATHER_API_KEY := "http://api.weatherapi.com/v1/forecast.json?key=45b012ede2e04e109f2172140241603&q=" + q + "&days=1&aqi=no&alerts=no"
+	if len(os.Args) >= 2 {
+		q = os.Args[1]
+	}
+
+	res, err := http.Get(WEATHER_API_KEY)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API not available")
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf("%s, %s: %0fF, %s\n",
+		location.Name,
+		location.Country,
+		current.TempF,
+		current.Condition.Text,
+	)
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		if date.Before(time.Now()) {
+			continue
+		}
+
+		message := fmt.Sprintf(
+			"%s - %.0fF, %.0f%%, %s\n",
+			date.Format("15:04"),
+			hour.TempF,
+			hour.ChanceOfRain,
+			hour.Condition.Text,
+		)
+
+		if hour.ChanceOfRain < 50 {
+			color.Green(message)
+		} else {
+			color.Red(message)
+
+		}
+
+	}
+
+}
